a041b31a8d49155a1b7e7790db2d294f194a936b
Lurnellian <Lurnelian@yandex.ru>
2017-12-01 01:32:45 +0300

    Изучал варианты кодировок

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,40 +1,14 @@
 import java.io.*;
 
-//public class Main {
-//    public static void main(String[] args) {
-//        if(args.length > 0) {
-//            for (int i = 0; i < args.length; i++) {
-//                try (BufferedReader bufferedReader= new BufferedReader(new FileReader(args[i]))) {
-//                    int c = 0;
-//                    while(bufferedReader.readLine()!=null) {
-//                        c++;
-//                    }
-//                    System.out.print("lines in " + args[i] + " equally " + c);
-//                    System.out.println();
-//                } catch (IOException ex) {
-//
-//                    System.out.println(ex.getMessage());
-//                }
-//            }
-//        }
-//        else System.out.println("No files selected");
-//    }
-//}
-
 public class Main {
-
     public static void main(String[] args) {
-        if (args.length>0) {
-            for (int i=0; i < args.length; i++) {
-                try (FileInputStream reader = new FileInputStream(args[i])) {
+        if(args.length > 0) {
+            for (int i = 0; i < args.length; i++) {
+                try (BufferedReader bufferedReader= new BufferedReader(new FileReader(args[i]))) {
                     int c = 0;
-                    int b;
-                    while ((b=reader.read())!=-1) {
-                        if (b==13)
-                            if (reader.read()==10)
-                                c++;
+                    while(bufferedReader.readLine()!=null) {
+                        c++;
                     }
-                    if (c!=0) c++;
                     System.out.print("lines in " + args[i] + " equally " + c);
                     System.out.println();
                 } catch (IOException ex) {
@@ -46,3 +20,29 @@ public class Main {
         else System.out.println("No files selected");
     }
 }
+
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        if (args.length>0) {
+//            for (int i=0; i < args.length; i++) {
+//                try (FileInputStream reader = new FileInputStream(args[i])) {
+//                    int c = 0;
+//                    int b;
+//                    while ((b=reader.read())!=-1) {
+//                        if (b==13)
+//                            if (reader.read()==10)
+//                                c++;
+//                    }
+//                    if (c!=0) c++;
+//                    System.out.print("lines in " + args[i] + " equally " + c);
+//                    System.out.println();
+//                } catch (IOException ex) {
+//
+//                    System.out.println(ex.getMessage());
+//                }
+//            }
+//        }
+//        else System.out.println("No files selected");
+//    }
+//}
